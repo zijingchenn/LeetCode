14e26fcfbd94cca2945e4b3ebc960da8938e8b1a
Zijing Chen <zijingchen@link.cuhk.edu.cn>
2025-02-27 23:15:37 -0500

    LC-merge-two-sorted-lists [Runtime: 0 ms; Memory: 17.7 MB]

---
+++ recursion/merge-two-sorted-lists.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # 虚拟头结点
+        dummy = ListNode(-1)
+        p = dummy
+        p1 = list1
+        p2 = list2
+        
+        while p1 is not None and p2 is not None: 
+
+            # 比较 p1 和 p2 两个指针
+            # 将值较小的的节点接到 p 指针
+            if p1.val > p2.val:
+                p.next = p2
+                p2 = p2.next
+            else:
+                p.next = p1
+                p1 = p1.next
+            # p 指针不断前进
+            p = p.next
+        
+        if p1 is not None:
+            p.next = p1
+        
+        if p2 is not None:
+            p.next = p2
+        
+        return dummy.next
