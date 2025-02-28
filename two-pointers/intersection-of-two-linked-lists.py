f4290e26c8d95d6294725a609d8ee39aef240c3c
Zijing Chen <zijingchen@link.cuhk.edu.cn>
2025-02-27 23:16:52 -0500

    LC-intersection-of-two-linked-lists [Runtime: 89 ms; Memory: 27.9 MB]

---
+++ two-pointers/intersection-of-two-linked-lists.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # p1 指向 A 链表头结点，p2 指向 B 链表头结点
+        p1, p2 = headA, headB
+        while p1 != p2:
+            # p1 走一步，如果走到 A 链表末尾，转到 B 链表
+            p1 = p1.next if p1 else headB
+            # p2 走一步，如果走到 B 链表末尾，转到 A 链表
+            p2 = p2.next if p2 else headA
+        return p1
