78bf903870ad3c321365deca087bf4230af53f9f
Zijing Chen <zijingchen@link.cuhk.edu.cn>
2025-02-27 23:16:21 -0500

    LC-remove-duplicates-from-sorted-list [Runtime: 0 ms; Memory: 18.1 MB]

---
+++ linked-list/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        slow, fast = head, head
+        while fast is not None:
+            if fast.val != slow.val:
+                # nums[slow] = nums[fast];
+                slow.next = fast
+                # slow++;
+                slow = slow.next
+            # fast++
+            fast = fast.next
+        # 断开与后面重复元素的连接
+        slow.next = None
+        return head
+        
+        
